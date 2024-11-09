08014ecbe3a5833f746d84844699f5ceec81e1aa
Aadil Khan <aadilmoid@gmail.com>
2024-11-09 14:02:17 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Logo ({ width = '100px' }) {
+  return <div className='w-16'><img src="https://images.pexels.com/photos/29323552/pexels-photo-29323552.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></div>
+}
+
+export default Logo
